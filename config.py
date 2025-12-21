470fc304ef179cb5c99e5c97567acd08c3531373
Howtog41 <howtog41@gmail.com>
2025-12-21 17:20:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 MONGO_URI = 'mongodb+srv://terabox255:v42hEybBOzoEg2mr@cluster0.nakwhlt.mongodb.net/?appName=Cluster0'
 ADMIN_ID = 5018200809
-TOKEN = '7877886995:AAH_w_RZ-bzKZl2buaPNGg0N93NKFTWsdXs'
+TOKEN = '7105638751:AAFtym0GvkuRTKPH6vHfPt_kH8cxOvfCQG4'
