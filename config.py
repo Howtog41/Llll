c8935a88ead7a5d3c68d5619dd51e339245b0de7
Howtog41 <howtog41@gmail.com>
2025-12-24 21:17:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,12 @@
-MONGO_URI = ''
-ADMIN_ID = 5018200809
-TOKEN = ''
+import os
+
+# MongoDB URI
+MONGO_URI = os.getenv("MONGO_URI", "")
+
+# Telegram Bot Token
+TOKEN = os.getenv("BOT_TOKEN", "")
+
+# Admin ID (int me convert zaroori)
+ADMIN_ID = int(os.getenv("ADMIN_ID", "5018200809"))
+
+
